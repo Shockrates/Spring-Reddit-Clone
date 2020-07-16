7353f4b332cac291a326743801259f42fdc3bf6b
unknown <Sokratis@SOKRATIS.EPAL2016.LOCAL>
2020-07-16 16:30:38 +0300

    Added code to JwtProvider

--- src/main/java/com/example/springredditclone/security/JwtProvider.java
+++ src/main/java/com/example/springredditclone/security/JwtProvider.java
@@ -1,13 +1,37 @@
 package com.example.springredditclone.security;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+import javax.annotation.PostConstruct;
+
+import com.example.springredditclone.exception.SpringRedditException;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtProvider {
 
+	private KeyStore keyStore;
+
+	@PostConstruct
+	public void init(){
+		try {
+			keyStore = KeyStore.getInstance("JKS");
+			InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
+			keyStore.load(resourceAsStream, "secret".toCharArray() );
+		} catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+			throw new SpringRedditException("Exception occurred while loading keystore");
+		}
+	}
+
 	public String generateToken(Authentication authenticate) {
 		return null;
 	}
+
+	private 
 
 }
