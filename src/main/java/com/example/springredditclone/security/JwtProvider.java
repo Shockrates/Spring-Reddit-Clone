8551b06fe19e2921dbfbc014ae608f2a21c39636
unknown <Sokratis@SOKRATIS.EPAL2016.LOCAL>
2020-07-17 15:01:52 +0300

    Completed JwtProvider

--- src/main/java/com/example/springredditclone/security/JwtProvider.java
+++ src/main/java/com/example/springredditclone/security/JwtProvider.java
@@ -1,5 +1,6 @@
 package com.example.springredditclone.security;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.*;
@@ -10,7 +11,10 @@ import javax.annotation.PostConstruct;
 import com.example.springredditclone.exception.SpringRedditException;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
 
 @Service
 public class JwtProvider {
@@ -21,17 +25,41 @@ public class JwtProvider {
 	public void init(){
 		try {
 			keyStore = KeyStore.getInstance("JKS");
-			InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
-			keyStore.load(resourceAsStream, "secret".toCharArray() );
+			//InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
+			InputStream resourceAsStream = new FileInputStream("/springblog.jks");
+			keyStore.load(resourceAsStream, "secret".toCharArray());
+			//keyStore.load(new FileInputStream("/springblog.jks"), "secret".toCharArray());
+			
+			
 		} catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
 			throw new SpringRedditException("Exception occurred while loading keystore");
 		}
 	}
 
-	public String generateToken(Authentication authenticate) {
-		return null;
+	private PrivateKey getPrivateKey() {
+		try {
+			System.out.println("Pivate Key from getPrivateKey= "+keyStore.getKey("springblog", "secret".toCharArray()));
+			return (PrivateKey)keyStore.getKey("springblog", "secret".toCharArray());
+		} catch (KeyStoreException | NoSuchAlgorithmException |UnrecoverableKeyException e) {
+			throw new SpringRedditException("Exception occured while retrieving public key from keystore");
+			
+		} 
+		
 	}
+	
 
-	private 
+	public String generateToken(Authentication authentication) {
+		org.springframework.security.core.userdetails.User principal = (User)authentication.getPrincipal();
+		//System.out.println("Rivate Key = "+getPrivateKey());
+		return Jwts.builder()
+		.setSubject(principal.getUsername())
+		.signWith(getPrivateKey())
+		.compact();
+	}
+	
+	
 
+	
+
+	
 }
